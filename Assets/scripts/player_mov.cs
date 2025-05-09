ac42779ba21c820d132c3853f1c37e6e79bcd897
tgarzagut <teresa.garzagut@gmail.com>
2025-05-09 00:10:27 -0500

    decorated world

--- Assets/scripts/player_mov.cs
+++ Assets/scripts/player_mov.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class player_mov : MonoBehaviour
+public class UnderwaterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+    public float verticalSpeed = 3f;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Input
+        float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right
+        float moveZ = Input.GetAxis("Vertical");   // W/S or Up/Down
+        float moveY = 0f;
+
+        // Vertical movement
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            moveY = 1f; // Go up
+        }
+        else if (Input.GetKey(KeyCode.CapsLock) || Input.GetKey(KeyCode.C))
+        {
+            moveY = -1f; // Go down
+        }
+
+        Vector3 movement = new Vector3(moveX, moveY * verticalSpeed / speed, moveZ);
+        rb.velocity = transform.TransformDirection(movement) * speed;
     }
 }
